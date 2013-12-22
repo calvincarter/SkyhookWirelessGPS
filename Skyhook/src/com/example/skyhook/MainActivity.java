c72407329aacc22f0ff9908b4cd35dc9fdc919c7
Calvin <Calvin@192.168.0.105>
2013-12-22 01:11:10 -0600

    Added Tiling function. This will help increase GPS accuracy

--- Skyhook/src/com/example/skyhook/MainActivity.java
+++ Skyhook/src/com/example/skyhook/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.skyhook;
 
+import java.io.File;
+
 import gps.MyWPSLocationCallback;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 
@@ -43,9 +46,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setup();
         
+        // Set up GPS and sign-in
+        setupAuthentification();
         
+        // Saves GPS map to phone, so app doesn't
+        // continuously contact server for GPS map coordinates
+        // This is a huge help!!!
+        setupTiling();
+               
         tv_location = (TextView) findViewById(R.id.textView1);
         tog_bt_accuracy = (ToggleButton) findViewById(R.id.toggleButton1);
         bt_startGPS = (Button) findViewById(R.id.button1);
@@ -54,9 +63,40 @@ public class MainActivity extends Activity {
     }
     
     // Initialize XPS class and set project API key
-    public void setup() {
+    private void setupAuthentification() {
     	_xps = new XPS(this);
     	_xps.setKey(_key);	
+    }
+    
+    // The tiling mode is a mix of the network and device-centric mode. It downloads, 
+    // from the server, a small portion of the database so the device can automonously 
+    // determine its location, without further need to contact the server.
+    private void setupTiling() {
+	     // make directory to save skyhook location maps. In theory, you can save enough of them so that you do not need network access and can still determine location. Android localization always requires network access, even GPS requires network access
+	     // make dir
+	     File externalStorage = Environment.getExternalStorageDirectory();  // get path to sdcard
+	     File skyhookCacheDirectory = new File(externalStorage.getAbsolutePath()+"/skyhook/");
+	     skyhookCacheDirectory.mkdir();
+		  
+	     /*
+	     Setup tiling so location determination can be performed locally. 
+	     Tiles are typically less then 50KB in size, so to download an area of 3x3 tiles 
+	     for each session you would set maxDataSizePerSession to 450KB, i.e. 460,800. 
+	     It is recommended that maxDataSizePerSession be a factor of 2 - 10 smaller than 
+	     maxDataSizeTotal, so that tiles from several areas can be cached.
+
+		 Parameters:
+		 dirpath - the path to a directory to store tiles.
+		 maxDataSizePerSession - the maximum size of data downloaded per session, in bytes. A value of 0 disables any further tile downloads.
+		 maxDataSizeTotal - the maximum size of all stored tiles, in bytes. A value of 0 causes all stored tiles to be deleted.
+		 listener - the callback function to control the tile download. By default, all tiles are downloaded.
+	     */
+
+	     // set up tiling
+	     int _maxDataSizePerSession = 100000000;  // a lot of data, so we should get everything we need. 
+	     int _maxDataSizeTotal = _maxDataSizePerSession;
+	     
+	     _xps.setTiling(skyhookCacheDirectory.getAbsolutePath(), _maxDataSizePerSession, _maxDataSizeTotal,  null); 
     }
     
     // When user clicks on button, start to get GPS locations
